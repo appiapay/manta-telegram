52f9d4b81d85356a982bccea4cd8b5e3b9d5868c
Alessandro Viganò <alvistar@gmail.com>
2018-10-17 19:17:10 +0200

    Solved name collision

--- mantatelegram/settings.py
+++ mantatelegram/settings.py
@@ -1,6 +1,6 @@
-from . import secrets
+from . import private
 
-BOT_TOKEN = secrets.BOT_TOKEN
+BOT_TOKEN = private.BOT_TOKEN
 MANTA_APP_ID = "72110a96-435c-447d-ae58-1133c1d25ce9"
-MANTA_APP_TOKEN = secrets.MANTA_APP_TOKEN
+MANTA_APP_TOKEN = private.MANTA_APP_TOKEN
 MANTA_HOST = "localhost"

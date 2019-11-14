f2b67aaee73462bdbae90e3000a4373beb7f5157
Alessandro Viganò <alvistar@gmail.com>
2019-11-14 09:40:03 +0100

    Changed settings to env

--- manta_telegram/settings.py
+++ manta_telegram/settings.py
@@ -1,14 +1,10 @@
-from . import private
+import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = private.BOT_TOKEN
-MANTA_APP_ID = "72110a96-435c-447d-ae58-1133c1d25ce9"
+load_dotenv()
 
-# EXPERIMENTAL
-#MANTA_APP_ID = "fe58ab4d-e201-4899-a46e-ad1746a2cdec"
-MANTA_APP_TOKEN = private.MANTA_APP_TOKEN
-#MANTA_HOST = "experimental.beappia.com"
-
-# COINGATE TEST
-MANTA_HOST = "127.0.0.1"
-
-LOGO = "appia-circle.png"
+BOT_TOKEN = os.environ['BOT_TOKEN']
+MANTA_APP_ID = os.environ['MANTA_APP_ID']
+MANTA_APP_TOKEN = os.environ['MANTA_APP_TOKEN']
+MANTA_HOST = os.environ['MANTA_HOST']
+LOGO = os.getenv('LOGO', 'appia-circle.png')

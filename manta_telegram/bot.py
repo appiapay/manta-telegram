86d14b454866c4ce424c5e2e8f581ba1073ce75e
Alessandro Viganò <alvistar@gmail.com>
2019-12-17 16:23:31 +0100

    Updated help text

--- manta_telegram/bot.py
+++ manta_telegram/bot.py
@@ -18,9 +18,17 @@ bot = Bot(token=settings.BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(commands=["start", "help"])
 async def send_welcome(message: types.Message):
-    await message.reply("Welcome to Appia Donation Bot\nPlease type \pay [amount] for your donation")
+    await message.reply(
+        "** Appia Donation Bot **\n"
+        "This bot is powered by Appia and Manta protocol. "
+        "You need a Manta-enabled wallet (ex. Natrium) for paying.\n"
+        "Donations will be sent to no profit organizations."
+        "\n"
+        "Donations:"
+        "  /pay {euro} - Make a donation - ex. /pay 0.01"
+    )
 
 
 async def wait_confirmation(message: types.Message, store: Store):
@@ -30,7 +38,7 @@ async def wait_confirmation(message: types.Message, store: Store):
             await message.reply("Payment Complete")
             return
         elif ack.status == Status.CONFIRMING:
-            await message.reply('Payment is Confirming')
+            await message.reply("Payment is Confirming")
             await wait_confirmation(message, store)
             return
         elif ack.status == Status.INVALID:
@@ -38,11 +46,11 @@ async def wait_confirmation(message: types.Message, store: Store):
             return
 
 
-@dp.message_handler(commands=['pay'])
+@dp.message_handler(commands=["pay"])
 @dp.async_task
 async def qr_code(message: types.Message):
     amount: Decimal = Decimal(message.get_args())
-    logger.info ("Creating a request for amount {}".format(amount))
+    logger.info("Creating a request for amount {}".format(amount))
     store = Store(settings.MANTA_APP_ID, host=settings.MANTA_HOST)
     store.mqtt_client.username_pw_set(settings.MANTA_APP_ID, settings.MANTA_APP_TOKEN)
     reply = await store.merchant_order_request(amount, "EUR")
@@ -51,14 +59,17 @@ async def qr_code(message: types.Message):
 
     with io.BytesIO() as output:
         if logo == "":
-            with path('manta_telegram', "appia-circle.png") as fn:
+            with path("manta_telegram", "appia-circle.png") as fn:
                 make_logo_qr(reply.url, fn, output)
         else:
             make_logo_qr(reply.url, logo, output)
         # image.save(output, format="png")
-        await message.reply_photo(output.getvalue(),
-                                  caption=f'<a href="{settings.API_BASEURL}/manta/{store.session_id}">'
-                                          f'{reply.url}</a>', parse_mode='HTML')
+        await message.reply_photo(
+            output.getvalue(),
+            caption=f'<a href="{settings.API_BASEURL}/manta/{store.session_id}">'
+            f"{reply.url}</a>",
+            parse_mode="HTML",
+        )
         await wait_confirmation(message, store)
 
 
@@ -67,6 +78,5 @@ def run():
     executor.start_polling(dp)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
-
